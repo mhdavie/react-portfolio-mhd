eea324220d6ccc1c7e4ad53bf3eee198f1f7d111
mdavie <marianahodniki1991@gmail.com>
2022-01-31 21:07:54 -0700

    about me component done

--- react-portfolio-mhd/src/App.js
+++ react-portfolio-mhd/src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Resume from "./components/Resume";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <main>
+    <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
       <About />
       <Projects />
-      <Contact />
+      <Resume/>
+      <Contact/>
+      <Footer/>
     </main>
   );
 }
-
