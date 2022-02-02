340e64da443f008d2f265fd594d5c4ad2c34c2f5
mdavie <marianahodniki1991@gmail.com>
2022-02-02 15:52:10 -0700

    git hub icon to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 function Footer() {   
     return (
     <footer className="footer bg-dark fixed-bottom">
-        <a href="https://github.com/mhdavie"><img src="" alt="Github" className="icon"/></a>
+        <a href="https://github.com/mhdavie"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Github" className="icon"/></a>
         <a href="https://www.linkedin.com/in/mariana-davie-33a873172/"><img src="" alt="LinkedIn" className="icon"/></a>
         <a href="https://twitter.com/MarianaDavie"><img src="" alt="Twitter" className="icon"/></a>
     </footer>
