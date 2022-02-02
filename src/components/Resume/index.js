3c4c4fa154643fe9928c9224696e0b68548a290b
mdavie <marianahodniki1991@gmail.com>
2022-02-02 09:56:14 -0700

    resume component

---
+++ src/components/Resume/index.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+function Resume() {
+  return (
+    <section class="container">
+      <h2 className="top-title">Resume</h2>
+      <hr></hr>
+
+      <div>
+        <div class="mt-5">
+          <h2 className="top-title">Mariana Davie</h2>
+          <ul>
+            <li>
+              Core competencies in enterpirse content management, full-stack web
+              development, data architecture, and managing integrations.
+            </li>
+          </ul>
+
+          <p class="mt-5">
+            <a href="https://www.linkedin.com/in/mariana-davie-33a873172/">
+              <img
+                src=""
+                alt="inkedIn"
+              />
+            </a>
+          </p>
+
+          <a
+            href=""
+            class="link"
+          >
+            Download Resume
+          </a>
+        </div>
+      </div>
+
+      <div class="justify-content-center mt-5">
+        <div>
+          <h2 className="top-title">Proficiencies</h2>
+          <ul>
+            <li>
+         
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Resume;
