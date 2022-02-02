2a2d0ce0f2c6569f9315efdb609b3dd9064e639b
mdavie <marianahodniki1991@gmail.com>
2022-02-02 09:30:38 -0700

    portfolio component added +routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom/cjs/react-router-dom.min';
-// import Header from './components/Header';
-// import Footer from './components/Footer';
+//import { HashRouter as Router, Route } from 'react-router-dom/cjs/react-router-dom.min';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import About from "./components/About"
-import Footer from './components/Footer';
+// import About from "./components/About"
+// import Footer from './components/Footer';
 
 
 function App() {
   return (
     <div>
-      <Router>
+      {/* <Router>
         <Route exact path="/" component={About}/>
         <Route path="/about" component={About}/>
         <Route path ="/" component = {Footer}/>
         <Route path ="/footer" component = {Footer}/>
+
+      </Router> */}
         
-      </Router>
+      <Header />
+      <Footer/>
+    
     </div>
   );
 }
