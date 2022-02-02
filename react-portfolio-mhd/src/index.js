a825b7e6618f4de141dc9aacbb6655f1c2f129ed
mdavie <marianahodniki1991@gmail.com>
2022-02-01 22:15:22 -0700

    working on render problem - src

--- react-portfolio-mhd/src/index.js
+++ react-portfolio-mhd/src/index.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from '../src/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
