5d06e6560764dffcfa6b52ec8f78687f770f71b4
Mariana Hodniki Davie <marianahodniki1991@gmail.com>
2022-05-03 14:12:28 -0600

    footer update

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 function Footer() {   
     return (
-    <footer className="footer bg-dark fixed-bottom">
+    <footer className="footer  fixed-bottom">
         <a href="https://github.com/mhdavie"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Github" className="icon"/></a>
         <a href="https://www.linkedin.com/in/mariana-davie-33a873172/"><img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn" className="icon"/></a>
         <a href="https://twitter.com/MarianaDavie"><img src="https://img.icons8.com/fluent/48/000000/twitter.png" alt="Twitter" className="icon"/></a>
