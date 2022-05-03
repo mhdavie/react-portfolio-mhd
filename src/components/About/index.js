d0c24c00174b4a84399dca6f504e8c011c1febab
Mariana Hodniki Davie <marianahodniki1991@gmail.com>
2022-05-03 14:32:17 -0600

    typo

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -14,7 +14,7 @@ function About() {
       
                 <h4 className="nav-title-font">Mariana Davie</h4>
                 <p >After 10+ years of experience as Portuguese Teacher/ Special Ed. Teacher I decided that was time 
-                    to change carreers and become a Web Developer.</p>   
+                    to change careers and become a Web Developer.</p>   
               
                 <p >I am currently a student at University of Denver Full-Stack Developer Bootcamp. 
 
